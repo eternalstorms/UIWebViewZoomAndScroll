6d925aebe17d94e15db98047a1d109d7f4cb72b6
Matthias Gansrigler <opensource@eternalstorms.at>
2015-01-29 12:26:13 +0100

    referenced blog in code

--- UIWebViewZoomAndScroll/UIWebViewZoomAndScroll/ViewController.m
+++ UIWebViewZoomAndScroll/UIWebViewZoomAndScroll/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Matthias Gansrigler on 29.01.2015.
 //  Copyright (c) 2015 Eternal Storms Software. All rights reserved.
-//
+// https://eternalstorms.wordpress.com/2015/01/29/ios-8-safari-action-extension-uiwebview-zoom-scale-scroll-position-and-javascript/
 
 #import "ViewController.h"
 
